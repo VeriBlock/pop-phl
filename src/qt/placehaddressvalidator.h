cf9d4e55bcb827922c65c1f90a4341be3fa822ec
Xagau <bullionxpress@gmail.com>
2020-11-04 12:41:01 -0500

    push with revised gitignore

---
+++ src/qt/placehaddressvalidator.h
@@ -0,0 +1,35 @@
+// Copyright (c) 2011-2014 The Placeholders Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef PLACEH_QT_PLACEHADDRESSVALIDATOR_H
+#define PLACEH_QT_PLACEHADDRESSVALIDATOR_H
+
+#include <QValidator>
+
+/** Base58 entry widget validator, checks for valid characters and
+ * removes some whitespace.
+ */
+class PlaceholdersAddressEntryValidator : public QValidator
+{
+    Q_OBJECT
+
+public:
+    explicit PlaceholdersAddressEntryValidator(QObject *parent);
+
+    State validate(QString &input, int &pos) const;
+};
+
+/** Placeholders address widget validator, checks for a valid placeh address.
+ */
+class PlaceholdersAddressCheckValidator : public QValidator
+{
+    Q_OBJECT
+
+public:
+    explicit PlaceholdersAddressCheckValidator(QObject *parent);
+
+    State validate(QString &input, int &pos) const;
+};
+
+#endif // PLACEH_QT_PLACEHADDRESSVALIDATOR_H

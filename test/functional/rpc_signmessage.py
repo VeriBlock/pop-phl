38f3b95921ea8fbad9cad9de37b5919958eb06f8
Bohdan Vanieiev <warchantua@gmail.com>
2020-11-05 18:08:03 +0200

    Fix test

--- test/functional/rpc_signmessage.py
+++ test/functional/rpc_signmessage.py
@@ -22,7 +22,7 @@ class SignMessagesTest(PlaceholdersTestFramework):
         self.log.info('test signing with priv_key')
         priv_key = 'cUeKHd5orzT3mz8P9pxyREHfsWtVfgsfDjiZZBcjUBAaGk1BTj7N'
         address = 'mpLQjfK79b7CCV4VMJWEWAj5Mpx8Up5zxB'
-        expected_signature = 'INbVnW4e6PeRmsv2Qgu8NuopvrVjkcxob+sX8OcZG0SALhWybUjzMLPdAsXI46YZGb0KQTRii+wWIQzRpG/U+S0='
+        expected_signature = 'IPZALnP6UStpsgkqNblcgDgUuiXWAxPLxUX859d4xTlbKaoLffWcwhT7TeXTLuDWy4OIgUmn0MZY9WV5GEQIfOM='
         signature = self.nodes[0].signmessagewithprivkey(priv_key, message)
         assert_equal(expected_signature, signature)
         assert self.nodes[0].verifymessage(address, signature, message)
